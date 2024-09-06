81c8223b19e57ff6f15431fffa9ec629e4eae914
JustJuulia <102248096+JustJuulia@users.noreply.github.com>
2024-09-06 14:54:56 +0200

    Added showing forbidden dates
    yay

--- HaveASeat-frontend/src/app/components/admin-dates/admin-dates.component.ts
+++ HaveASeat-frontend/src/app/components/admin-dates/admin-dates.component.ts
@@ -1,14 +1,16 @@
 import { NgIf } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { ForbiddenDate } from '../../models/models';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-admin-dates',
   standalone: true,
-  imports: [HttpClientModule, NgIf, FormsModule],
+  imports: [HttpClientModule, NgIf, FormsModule, NgFor],
   templateUrl: './admin-dates.component.html',
   styleUrls: ['./admin-dates.component.css']
 })
@@ -16,7 +18,7 @@ export class AdminDatesComponent {
   private addDate = 'https://localhost:7023/api/ForbiddenDate/AddForbiddenDate/';
   private deleteDate = 'https://localhost:7023/api/ForbiddenDate/delete/{date}';
   private getallDates = 'https://localhost:7023/api/ForbiddenDate/getAllForbiddenDates';
-
+  alldates: string[] = [];
   today: string;
   pickedDate: string | null = null;
   writtendescr: string | null = null;
@@ -25,7 +27,22 @@ export class AdminDatesComponent {
     this.today = new Date().toISOString().split('T')[0];
     this.pickedDate = this.today;
   }
-
+  ngOnInit(): void {
+    this.ShowAllForbiddenDates();
+  }
+  ShowAllForbiddenDates() :void{
+    this.http.get<ForbiddenDate[]>(this.getallDates).subscribe({
+      next: (dates: ForbiddenDate[]) => {
+        this.alldates = dates.map(date => 
+          `${new Date(date.date).toISOString().split('T')[0]} - ${date.description}`
+        );
+      },
+      error: (err) => {
+        console.error('Error during login:', err);
+        alert('There was an error during login. Please try again later.');
+      },
+    });
+  }
   CreateForbiddenDate(): void {
     if (this.pickedDate != null) {
       const dateObj = new Date(this.pickedDate);
