99d156c7b478a362aa394033ce5d33f1d181dfc3
amystas <oskaralama@gmail.com>
2024-09-06 14:17:25 +0200

    editor map
    i be placing blocks and shit cause im in fucking minecraft

--- HaveASeat-frontend/src/app/components/editor-map/editor-map.component.ts
+++ HaveASeat-frontend/src/app/components/editor-map/editor-map.component.ts
@@ -1,12 +1,106 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule, NgFor, NgStyle } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Desk, Room, Cell, Reservation } from '../../models/models';
+import { NgIf } from '@angular/common';
+import { forkJoin } from 'rxjs';
+import { HeaderComponent } from '../header/header.component';
+import { UserService } from '../../services/user.service';
+import { MapaService } from '../../services/mapa.service';
 
 @Component({
   selector: 'app-editor-map',
-  standalone: true,
-  imports: [],
   templateUrl: './editor-map.component.html',
-  styleUrl: './editor-map.component.css'
+  styleUrl: './editor-map.component.css',
+  imports: [NgStyle, NgFor, HttpClientModule, NgIf, CommonModule, HeaderComponent],
+  standalone: true,
+  providers: [MapaService, UserService],
 })
-export class EditorMapComponent {
+export class EditorMapComponent implements OnInit, OnChanges {
+  roomWidth = 20;
+  roomHeight = 13;
+  rooms: Room[] = [];
+  reservations: Reservation[] = [];
+  clickedOnce = false;
+  cellx: number = 0;
+  celly: number = 0;
+  previousCell: Cell | null = null;
+  @Input() selectedDate: string = ''; 
+  @Input() userId: number | null = null;
+
+  constructor(private mapaService: MapaService) {}
+  ngOnInit(): void {
+    forkJoin({
+      rooms: this.mapaService.getRooms(),
+    }).subscribe(
+      ({ rooms }) => {
+        this.rooms = rooms;
+        this.markDeskCells();
+      },
+      (error: any) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+  }
+
+
+
+
+  onDeskClick(cell: Cell) {
+    if(this.previousCell != null) {
+      this.previousCell.isClicked = false;
+    }
+    this.cellx = cell.positionX; 
+    this.celly = cell.positionY;
+    cell.isClicked = true;
+    this.previousCell = cell;
+  }
+
+  check(desk: Desk, cell: Cell): boolean {
+    return desk.positionX === cell.positionX && desk.positionY === cell.positionY;
+  }
+
+  getRotationClass(chairPosition: number): string {
+
+    switch (chairPosition) {
+      case 1:
+        return 'rotate-right';
+      case 2:
+        return 'rotate-bottom';
+      case 3:
+        return 'rotate-left';
+      default:
+        return '';
+    }
+  }
+
+  markDeskCells(): void {
+    if (this.rooms.length) {
+      this.rooms.forEach(room => {
+        room.cells.forEach(cell => {
+          cell.isDesk = room.desks.some(desk => this.check(desk, cell));
+
+          if (cell.isDesk) {
+            const desk = room.desks.find(desk => this.check(desk, cell));
+            if (desk) {
+              cell.rotationClass = this.getRotationClass(desk.chairPosition);
+            }
+          } else {
+          }
+        });
+      });
+    }
+  }
+
+  getBorder(roomId: number, positionY: number, positionX: number): string {
+    const room = this.rooms.find(r => r.id === roomId);
+    const cell = room?.cells.find(c => c.positionX === positionX && c.positionY === positionY);
+    return cell?.border ?? '';
+  }
+
 
 }
