2c0bc32999f42270d1893914a22df70fe6b99164
Julia <102248096+JustJuulia@users.noreply.github.com>
2024-09-06 10:23:53 +0200

    sign in and up works!!!!

--- HaveASeat-frontend/src/app/components/header/header.component.ts
+++ HaveASeat-frontend/src/app/components/header/header.component.ts
@@ -3,9 +3,7 @@ import { UserService } from '../../services/user.service';
 import { HttpClientModule } from '@angular/common/http';
 import { User } from '../../models/models';
 import { NgIf } from '@angular/common';
-import { DateService } from '../../services/date.service';
-import { ForbiddenDate } from '../../models/models';
-import { findIndex } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +11,7 @@ import { findIndex } from 'rxjs';
   imports: [HttpClientModule, NgIf],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'], 
-  providers: [UserService, DateService]
+  providers: [UserService]
 })
 export class HeaderComponent implements OnChanges, AfterViewInit {
 
@@ -24,15 +22,9 @@ export class HeaderComponent implements OnChanges, AfterViewInit {
   today: string;
   username: string = "";
   user: User = <User>{};
-  forbiddenDates: ForbiddenDate[] = []
 
-  constructor(private userService: UserService, private dateService: DateService) {
+  constructor(private userService: UserService, private router: Router) {
     this.today = new Date().toISOString().slice(0, 10);
-    dateService.getDates().subscribe({
-      next: dates => {
-        this.forbiddenDates = dates;
-      }
-    })
   }
 
   ngOnChanges() {
@@ -79,17 +71,12 @@ export class HeaderComponent implements OnChanges, AfterViewInit {
   onDateChange(event: any): void {
     const selectedDate = event.target.value;
     const day = new Date(selectedDate).getDay();
-    const holiday = this.forbiddenDates.find(h => h.date == selectedDate);
-    if(holiday != undefined) {
-      alert(holiday.description);
-      this.dateChanged.emit(this.today);
-
-    }
-    else if (day === 6 || day === 0) {
+    if (day === 6 || day === 0) {
       alert('W weekendy firma jest zamknieta');
       event.target.value = this.today;
       this.dateChanged.emit(this.today);
     }
+    
     else if (day != 6 && day != 0) {
       this.dateChanged.emit(selectedDate);
     }
@@ -106,6 +93,9 @@ export class HeaderComponent implements OnChanges, AfterViewInit {
       console.log(this.user.role);
       return false;
     }
+  }
+  GoToEdition(){
+    //this.router.navigate(['main'], { queryParams: { userId } });
   }
   dropdownVisible: boolean = false;
   toggleDropdown(state: boolean) {
