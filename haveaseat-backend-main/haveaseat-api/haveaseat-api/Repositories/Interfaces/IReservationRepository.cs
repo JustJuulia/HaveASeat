d093f64621dc7e07ce875c4c47e204eaa8f45e05
Qa31O <148481009+Qa31O@users.noreply.github.com>
2024-09-06 13:37:41 +0200

    Get all user from reservations
    Get all user from reservations

--- haveaseat-backend-main/haveaseat-api/haveaseat-api/Repositories/Interfaces/IReservationRepository.cs
+++ haveaseat-backend-main/haveaseat-api/haveaseat-api/Repositories/Interfaces/IReservationRepository.cs
@@ -9,4 +9,5 @@ public interface IReservationRepository
 
     Task<NewReservationDTO> InsertReservations(NewReservationDTO reservation);
     Task<NewReservationDTO> DeleteReservationById(long reservationId);
+    Task<List<UserDTO>> GetAllUsersFromReservationsByDate(DateOnly date);
 }
