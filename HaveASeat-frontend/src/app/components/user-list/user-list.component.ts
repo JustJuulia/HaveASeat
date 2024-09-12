7e0e4d42a2b976a966d338a933c13059339abe0e
JustJuulia <102248096+JustJuulia@users.noreply.github.com>
2024-09-12 12:02:16 +0200

    FIXED ALL ERRORS
    OMD FGSJBKUCFSK SO PROUD OF MYSELF stil have to optimize tho hehe

--- HaveASeat-frontend/src/app/components/user-list/user-list.component.ts
+++ HaveASeat-frontend/src/app/components/user-list/user-list.component.ts
@@ -3,6 +3,7 @@ import { NgIf, NgFor } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { User } from '../../models/models';
+import { ForbiddenDate } from '../../models/models';
 
 @Component({
   selector: 'app-user-list',
@@ -14,25 +15,59 @@ import { User } from '../../models/models';
 export class UserListComponent implements OnInit {
   today: string;
   users_list: string[] = [];
+  alldates: Date[] = [];
 
   private getAllUsersUrl = 'https://localhost:7023/api/Reservation/getAllUsersWithReservationByDay/';
+  private getallDates = 'https://localhost:7023/api/ForbiddenDate/getAllForbiddenDates';
 
   constructor(private http: HttpClient, private router: Router) {
     this.today = new Date().toISOString().split('T')[0];
   }
 
+  find_today(today: string): string {
+    let today_date = new Date(today);
+    while (true) {
+      let day = today_date.getDay();
+      let counter = 0;
+      if (day !== 6 && day !== 0) { 
+        this.alldates.forEach((forb_date: Date) => {
+          if (today_date.getTime() === forb_date.getTime()) {
+            counter++;
+          }
+        });
+        if (counter === 0) {
+          return today_date.toISOString().slice(0, 10); 
+        }
+      }
+      today_date.setDate(today_date.getDate() + 1);
+    }
+  }
+
   ngOnInit(): void {
-    this.getUsers(this.today);
+    this.http.get<ForbiddenDate[]>(this.getallDates).subscribe({
+      next: (dates: ForbiddenDate[]) => {
+        this.alldates = dates.map(date => 
+          new Date(date.date)
+        );
+        const today = new Date().toISOString().slice(0, 10);
+        const checked_today = this.find_today(today);
+        this.getUsers(checked_today);
+      },
+      error: (err: any) => {
+        console.error('Error during fetching forbidden dates', err);
+      },
+    });
   }
 
   getUsers(date: string): void {
-    const url = `${this.getAllUsersUrl}${date}`;
+    const checked_today = this.find_today(date);
+    const url = `${this.getAllUsersUrl}${checked_today}`;
     this.http.get<User[]>(url).subscribe({
       next: (users: User[]) => {
         this.users_list = users.map(user => `${user.name} ${user.surname}`);
       },
       error: (err) => {
-        console.error('Error during dates show', err);
+        console.error('Error during fetching users', err);
       },
     });
   }
